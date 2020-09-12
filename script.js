085c226f5af673dbfa4fb0aed616685e23094b36
Azrael-4 <70764617+Azrael-4@users.noreply.github.com>
2020-09-12 15:11:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var velocidade = 200;
+var optiones = 0;
+
+while (!(optiones == 1 || optiones == 2 || optiones == 3 || optiones == 4)) {
+  var x = prompt("Escolha a velocidade do jogo!\n\n1 - Lenta\n\n2 - Média\n\n3 - Rápida\n\n4 - Burlesca\n\n", "1 2 3 4");
+  optiones = x;
+  }
+
+if (optiones == 1) {velocidade = 200};
+if (optiones == 2) {velocidade = 150};
+if (optiones == 3) {velocidade = 100};
+if (optiones == 4) {velocidade = 50};
+
+
+
+
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale= 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
+
+(function setup() {
+  snake = new Snake();
+  fruit = new fruit();
+  
+  fruit.pickLocation();
+
+
+  window.setInterval( () => {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    fruit.draw();
+    snake.update();
+    snake.draw();
+
+    if (snake.eat(fruit)) {
+      fruit.pickLocation();
+    }
+
+    snake.checkCollision();
+
+    document.querySelector('.score').innerText = snake.total;
+
+    }, velocidade);
+}());
+
+//
+
+window.addEventListener('keydown', ((evt) => {
+  //console.log(evt);
+  const direction = evt.key.replace('Arrow','');
+  //console.log(direction);
+  snake.changeDirection(direction);
+
+}));
+
+function removerMsg () {
+  document.getElementById('SOM').innerHTML = "";
+}
